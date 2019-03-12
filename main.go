5a93493bdbd59e8bdde3b6437174f4361ae74bfe
Benjamin Tsai <hello@benjamintsai.com>
2019-03-12 00:46:28 -0700

    two possible cases of reordering

--- main.go
+++ main.go
@@ -1,58 +1,87 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"runtime"
 )
 
+const attempts = 10000
+
 func main() {
 	runtime.GOMAXPROCS(2)
 
+	obj := &demoRecord{}
+	testScenario("two int", obj.setTwoInt, obj.readTwoInt, obj.reset)
+	testScenario("int arr", obj.setIntArr, obj.readIntArr, obj.reset)
+}
+
+func testScenario(label string, set func(), get func() (bool, bool), reset func()) {
 	foundReorder := false
-	for !foundReorder {
-		obj := &twoVal{}
+	var cnt int
 
-		readOkCh := make(chan bool)
+	for cnt = 0; !foundReorder && cnt < attempts; cnt++ {
+		reset()
 
+		doneCh := make(chan struct{})
 		go func() {
-			obj.setup()
+			defer close(doneCh)
+			set()
 		}()
 
-		go func() {
-			done, err := obj.read()
-			for !done && err == nil {
-				done, err = obj.read()
-			}
-			if err != nil {
-				readOkCh <- false
-			}
-			readOkCh <- true
-		}()
+		done, ok := get()
+		for !done && !ok {
+			done, ok = get()
+		}
+		foundReorder = ok
 
-		res := <- readOkCh
-		fmt.Println(res)
+		<-doneCh
+	}
 
-		foundReorder = !res
+	if foundReorder {
+		fmt.Printf("%d - FOUND %s\n", cnt, label)
+	} else {
+		fmt.Printf("%d - NOT FOUND %s\n", cnt, label)
 	}
 }
 
-type twoVal struct {
-	val1 int
-	val2 int
+type demoRecord struct {
+	val1   int
+	val2   int
+	intArr []int
 }
 
-func (t *twoVal) setup() {
+func (t *demoRecord) reset() {
+	t.val1 = 0
+	t.val2 = 0
+	t.intArr = make([]int, 2)
+}
+
+func (t *demoRecord) setTwoInt() {
 	t.val1 = 1
 	t.val2 = 2
 }
 
-func (t *twoVal) read() (bool, error) {
+func (t *demoRecord) readTwoInt() (bool, bool) {
 	if t.val1 == 1 && t.val2 == 2 {
-		return true, nil
+		return true, false
 	}
-	if t.val1 != 1 && t.val2 == 2 {
-		return false, errors.New("")
+	if t.val2 == 2 && t.val1 != 1 {
+		return false, true
 	}
-	return false, nil
+	return false, false
+}
+
+func (t *demoRecord) setIntArr() {
+	t.intArr[0] = 1
+	t.intArr[1] = 2
+}
+
+func (t *demoRecord) readIntArr() (bool, bool) {
+	if t.intArr[0] == 1 && t.intArr[1] == 2 {
+		return true, false
+	}
+	if t.intArr[1] == 2 && t.intArr[0] != 1 {
+		return false, true
+	}
+	return false, false
 }
